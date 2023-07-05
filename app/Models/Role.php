dc4bd88ae2ab751ced9b5e9eb2e573253169f88b
tedi lesmana <tedi.lesmana0123@gmail.com>
2023-07-05 17:26:12 +0700

    update

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use SoftDeletes;
     use HasFactory;
     const CODE = 'A1-ROLES';
     protected $guarded = array('id');
--- database/migrations/2023_07_04_172651_create_roles_table.php
+++ database/migrations/2023_07_04_172651_create_roles_table.php
@@ -18,7 +18,6 @@ class CreateRolesTable extends Migration
             $table->char('role_code', 35)->unique();
             $table->string('description')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
